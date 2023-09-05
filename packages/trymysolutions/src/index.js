1968b4653801132b4761f19d38800a9a3f0adf02
9dv2674 <ydv@levi.com>
2023-09-05 18:47:47 +0530

    trymysol and keywords

---
+++ packages/trymysolutions/src/index.js
@@ -0,0 +1,9 @@
+import convertFetchToCurl from "convert-fetch-to-curl";
+import window from 'handle-window-undefined';
+import parseQueryParameters from 'parse-url-query-params';
+
+export default {
+    convertFetchToCurl,
+    window,
+    parseQueryParameters
+}
